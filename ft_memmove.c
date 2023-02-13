85e6aee787de2f602491b9c109064848ccbace71
David Boyer <daboyer@c3r5p5.42perpignan.fr>
2023-02-13 13:05:53 +0100

    update files

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: daboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 09:29:32 by daboyer           #+#    #+#             */
-/*   Updated: 2023/02/13 12:29:46 by daboyer          ###   ########.fr       */
+/*   Updated: 2023/02/13 13:04:23 by daboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	s = (unsigned char *)src;
 	if (d < s)
 		while (n--)
-			*d++ = *s--;
+			*d++ = *s++;
 	else
 	{
-		d += n - 1;
-		s += n - 1;
+		d = d + n - 1;
+		s = s + n - 1;
 		while (n--)
 			*d-- = *s--;
 	}
