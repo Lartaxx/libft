a33980e7d5061331bd1047459ca27c893aa6761c
David Boyer <daboyer@c3r5p5.42perpignan.fr>
2023-02-13 14:37:25 +0100

    add files

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,20 @@
 /*   By: daboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:39:27 by daboyer           #+#    #+#             */
-/*   Updated: 2022/11/09 17:39:39 by daboyer          ###   ########.fr       */
+/*   Updated: 2023/02/13 14:37:04 by daboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+	while (((char *)s1)[i] && ((char *)s1)[i] == ((char *)s2)[i] && n--)
 		i++;
-	}
-	return (0);
+	if (!n)
+		return (0);
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
