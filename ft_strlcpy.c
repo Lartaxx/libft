e505be576c7fea1736de4eab7642072d00a1bffc
David Boyer <daboyer@c3r5p5.42perpignan.fr>
2023-02-13 13:10:43 +0100

    update files

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,26 +6,26 @@
 /*   By: daboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 14:54:04 by daboyer           #+#    #+#             */
-/*   Updated: 2023/02/06 15:06:33 by daboyer          ###   ########.fr       */
+/*   Updated: 2023/02/13 13:10:33 by daboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	if (!dst || !src)
-		return (0);
-	while (src[i] && i < dstsize - 1)
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
 	}
-	if (dstsize)
-		dst[i] = '\0';
 	while (src[i])
 		i++;
 	return (i);
