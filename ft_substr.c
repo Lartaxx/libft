86c03fbb5bc9d8ed94b5c472c74bc7e832a2a97f
David Boyer <daboyer@c3r5p5.42perpignan.fr>
2023-02-15 11:31:06 +0100

    add files

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: daboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 13:32:39 by daboyer           #+#    #+#             */
-/*   Updated: 2023/02/14 13:55:14 by daboyer          ###   ########.fr       */
+/*   Updated: 2023/02/15 11:30:36 by daboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
-	size_t	s_len;
+	char	*sub;
+	size_t	i;
 
-	if (s == 0)
-		return (0);
-	s_len = ft_strlen(s);
-	if (start > s_len)
-		return(ft_strdup(""));
-	if (len > s_len - start)
-		len = s_len - start;
-
-	substr = (char *)malloc(sizeof(char *) * (len + 1));
-	if (substr == 0)
-		return (0);
-	ft_memcpy(substr, s + start, len);
-	substr[len] = '\0';
-	return (substr);
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
